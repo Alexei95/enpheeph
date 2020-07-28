17fc686a35bb35077604dd2b263c7712cc8974d3
Alessio Colucci <alessio.colucci@gmail.com>
2020-07-28 23:37:32 +0200

    Improve implementation of samplers.
    Now BaseSampler implements abstract methods, such as iter_choice,
    which returns an iterator when choosing elements from a set of indices,
    and torch_sample which samples the generator and returns
    a compatible torch.Tensor.

--- src/fi/samplers/basesampler.py
+++ src/fi/samplers/basesampler.py
@@ -1,9 +1,13 @@
+import abc
+
 import numpy.random
+import torch
 
 from ...common import DEFAULT_PRNG, DEFAULT_PRNG_SEED
 
+
 class BaseSampler(object):
-    def __init__(self, bit_generator=DEFAULT_PRNG, seed=DEFAULT_PRNG_SEED, *args, **kwargs):
+    def __init__(self, seed=DEFAULT_PRNG_SEED, bit_generator=DEFAULT_PRNG, *args, **kwargs):
         super().__init__()
 
         self._seed = seed
@@ -14,3 +18,15 @@ class BaseSampler(object):
     @property
     def prng(self):
         return self._prng
+
+    # this set of arguments is the standard ones, but they could be different
+    # so we use also *args, **kwargs
+    @abc.abstractmethod
+    def iter_choice(self, high=None, n_elements=None, low=None, unique=None, *args, **kwargs):
+        return iter([])
+
+    # this set of arguments is the standard ones, but they could be different
+    # so we use also *args, **kwargs
+    @abc.abstractmethod
+    def torch_sample(self, high=None, shape=None, low=None, dtype=None, device=None, *args, **kwargs):
+        return torch.zeros(())
