1aa2239deb3005f259ab6756d3e9657144efaee8
Alessio Colucci <alessio.colucci@gmail.com>
2020-07-30 18:14:05 +0200

    Implement base DataModule for datasets.
    Define common constants for datasets.

--- src/dnn/datasets/basedatamodule.py
+++ src/dnn/datasets/basedatamodule.py
@@ -14,26 +14,47 @@ from .common import *
 class BaseDataModule(pl.LightningDataModule):
     def __init__(self, path=DEFAULT_DATASET_PATH,
 
-                       train_transform=None,
+                       name=None,
+
+                       dataset_class=None,
+
+                       train_transform=DEFAULT_TRAIN_TRANSFORM,
                        train_normalize=DEFAULT_TRAIN_NORMALIZE,
                        train_batch_size=DEFAULT_TRAIN_BATCH_SIZE,
                        train_percentage=DEFAULT_TRAIN_PERCENTAGE,
+                       train_shuffle=DEFAULT_TRAIN_SHUFFLE,
+                       train_num_workers=DEFAULT_TRAIN_NUM_WORKERS,
+                       train_pin_memory=DEFAULT_TRAIN_PIN_MEMORY,
+                       train_drop_incomplete_batch=DEFAULT_TRAIN_DROP_INCOMPLETE_BATCH,
+                       train_batch_timeout=DEFAULT_TRAIN_BATCH_TIMEOUT,
 
-                       val_transform=None,
-                       val_normalize=DEFAULT_VAL_NORMALIZE,
+                       val_transform=DEFAULT_VALIDATION_TRANSFORM,
+                       val_normalize=DEFAULT_VALIDATION_NORMALIZE,
                        val_batch_size=DEFAULT_VALIDATION_BATCH_SIZE,
                        val_percentage=DEFAULT_VALIDATION_PERCENTAGE,
+                       val_shuffle=DEFAULT_VALIDATION_SHUFFLE,
+                       val_num_workers=DEFAULT_VALIDATION_NUM_WORKERS,
+                       val_pin_memory=DEFAULT_VALIDATION_PIN_MEMORY,
+                       val_drop_incomplete_batch=DEFAULT_VALIDATION_DROP_INCOMPLETE_BATCH,
+                       val_batch_timeout=DEFAULT_VALIDATION_BATCH_TIMEOUT,
 
-                       test_transform=None,
+                       test_transform=DEFAULT_TEST_TRANSFORM,
                        test_normalize=DEFAULT_TEST_NORMALIZE,
                        test_batch_size=DEFAULT_TEST_BATCH_SIZE,
                        test_percentage=DEFAULT_TEST_PERCENTAGE,
+                       test_shuffle=DEFAULT_TEST_SHUFFLE,
+                       test_num_workers=DEFAULT_TEST_NUM_WORKERS,
+                       test_pin_memory=DEFAULT_TEST_PIN_MEMORY,
+                       test_drop_incomplete_batch=DEFAULT_TEST_DROP_INCOMPLETE_BATCH,
+                       test_batch_timeout=DEFAULT_TEST_BATCH_TIMEOUT,
 
                        *args,
                        **kwargs):
         super().__init__(*args, **kwargs)
 
         self._path = pathlib.Path(path).resolve()
+        self._name = name
+        self._dataset_class = dataset_class
 
         self._train_transform = self.setup_transform(transform=train_transform,
                                                      normalize=train_normalize,
@@ -41,6 +62,12 @@ class BaseDataModule(pl.LightningDataModule):
         self._train_normalize = train_normalize
         self._train_percentage = train_percentage
         self._train_batch_size = train_batch_size
+        self._train_shuffle = train_shuffle
+        self._train_num_workers = train_num_workers
+        self._train_pin_memory = train_pin_memory
+        self._train_drop_incomplete_batch = train_drop_incomplete_batch
+        self._train_batch_timeout = train_batch_timeout
+        self._train_dataset = None
 
         self._val_transform = self.setup_transform(transform=val_transform,
                                                      normalize=val_normalize,
@@ -48,6 +75,12 @@ class BaseDataModule(pl.LightningDataModule):
         self._val_normalize = val_normalize
         self._val_percentage = val_percentage
         self._val_batch_size = val_batch_size
+        self._val_shuffle = val_shuffle
+        self._val_num_workers = val_num_workers
+        self._val_pin_memory = val_pin_memory
+        self._val_drop_incomplete_batch = val_drop_incomplete_batch
+        self._val_batch_timeout = val_batch_timeout
+        self._val_dataset = None
 
         self._test_transform = self.setup_transform(transform=test_transform,
                                                      normalize=test_normalize,
@@ -55,6 +88,20 @@ class BaseDataModule(pl.LightningDataModule):
         self._test_normalize = test_normalize
         self._test_batch_size = test_batch_size
         self._test_percentage = test_percentage
+        self._test_shuffle = test_shuffle
+        self._test_num_workers = test_num_workers
+        self._test_pin_memory = test_pin_memory
+        self._test_drop_incomplete_batch = test_drop_incomplete_batch
+        self._test_batch_timeout = test_batch_timeout
+        self._test_dataset = None
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def name(self):
+        return self._name
 
     @staticmethod
     def setup_transform(transform=None, normalize=DEFAULT_NORMALIZE, default=DEFAULT_TRANSFORM):
@@ -67,10 +114,34 @@ class BaseDataModule(pl.LightningDataModule):
         return transform
 
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=64)
+        if self._train_dataset is None:
+            raise Exception('train dataset must be initialized')
+        return torch.utils.data.DataLoader(self._train_dataset,
+                                           batch_size=self._train_batch_size,
+                                           num_workers=self._train_num_workers,
+                                           pin_memory=self._train_pin_memory,
+                                           shuffle=self._train_shuffle,
+                                           drop_last=self._train_drop_incomplete_batch,
+                                           timeout=self._train_batch_timeout)
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=64)
+        if self._val_dataset is None:
+            raise Exception('validation dataset must be initialized')
+        return torch.utils.data.DataLoader(self._val_dataset,
+                                           batch_size=self._val_batch_size,
+                                           num_workers=self._val_num_workers,
+                                           pin_memory=self._val_pin_memory,
+                                           shuffle=self._val_shuffle,
+                                           drop_last=self._val_drop_incomplete_batch,
+                                           timeout=self._val_batch_timeout)
 
     def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=64)
+        if self._test_dataset is None:
+            raise Exception('test dataset must be initialized')
+        return torch.utils.data.DataLoader(self._test_dataset,
+                                           batch_size=self._test_batch_size,
+                                           num_workers=self._test_num_workers,
+                                           pin_memory=self._test_pin_memory,
+                                           shuffle=self._test_shuffle,
+                                           drop_last=self._test_drop_incomplete_batch,
+                                           timeout=self._test_batch_timeout)
