5bf6accbc90d66b33102ee1255f83ff2f733c00b
Alessio Colucci <alessio.colucci@gmail.com>
2020-08-07 12:42:09 +0200

    Convert the base module to PyTorch Lightning.
    This module now includes training and testing, and supports the
    dataloaders. It can be inherited to implement more models.

--- src/dnn/models/basemodule.py
+++ src/dnn/models/basemodule.py
@@ -3,44 +3,52 @@ import abc
 import torch
 import pytorch_lightning as pl
 
+# FIXME: for now we are using self.logger for logging the training and
+# testing steps, but in reality self.logger can be a list containing multiple
+# loggers and this case must be handled accordingly
+
+# FIXME: add loggers (TensorBoard) and Python logging
+
 class BaseModule(pl.LightningModule):
 
-    def __init__(self, loss, *args, **kwargs):
+    def __init__(self, loss, optimizer_class, optimizer_args, *args, **kwargs):
+        '''Here we save all the useful settings, like a loss which must be a
+        function, accepting predictions and targets in this order.'''
         super().__init__(*args, **kwargs)
+
+        self._loss = loss
+        self._optimizer_class = optimizer_class
+        self._optimizer_args = optimizer_args
+
+        self.save_hyperparameters()
 
     @abc.abstractmethod
     def forward(self, *args, **kwargs):
+        '''This defines the forward method for getting predictions from a test
+        input. It is an abstract method, so it cannot be instantiated directly
+        but it must be subclassed.'''
         return None
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
-        logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
+        predictions = self.forward(x)
+        loss = self._loss(predictions, y)
 
-        logs = {'train_loss': loss}
-        return {'loss': loss, 'log': logs}
+        return loss
 
     def test_step(self, test_batch, batch_idx):
         x, y = test_batch
-        logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
-        print(loss)
-        return {'test_loss': loss}
+        predictions = self.forward(x)
+        loss = self._loss(predictions, y)
+        return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
-        logits = self.forward(x)
-        loss = self.cross_entropy_loss(logits, y)
-        return {'val_loss': loss}
-
-    def validation_epoch_end(self, outputs):
-        # called at the end of the validation epoch
-        # outputs is an array with what you returned in validation_step for each batch
-        # outputs = [{'loss': batch_0_loss}, {'loss': batch_1_loss}, ..., {'loss': batch_n_loss}]
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        tensorboard_logs = {'val_loss': avg_loss}
-        return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
+        predictions = self.forward(x)
+        loss = self._loss(predictions, y)
+        return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = self._optimizer_class(self.parameters(), **self._optimizer_args)
+        # different optimizers can be passed as a tuple
         return optimizer
