e6d60580efd23edca0e4ce03a9262de847b4ca10
Alessio Colucci <alessio.colucci@gmail.com>
2020-08-13 20:20:55 +0200

    Change injectors.

--- src/fi/injectors/activationfaultinjector.py
+++ src/fi/injectors/activationfaultinjector.py
@@ -2,13 +2,15 @@ from . import faultinjectorabc
 
 
 class ActivationFaultInjector(faultinjectorabc.FaultInjectorABC):
-    def __init__(self, sampler, injector, *args, **kwargs):
-        super().__init__(*args, sampler=sampler, injector=injector, **kwargs)
+    def __init__(self, injection_module, *args, **kwargs):
+        super().__init__(*args, injection_module=injection_module, **kwargs)
 
-        self._sampler = sampler
-        self._injector = injector
+        self._injection_module = injection_module
 
     def update_module(self, parent_module, target_module_name):
-        #
+        # we get the original module and we return 
         module = getattr(parent_module, target_module_name)
-        sample =
+        new_module = torch.nn.Sequential(module, self._injection_module)
+        return new_module
+
+FAULT_INJECTOR = {ActivationFaultInjector.__name__: ActivationFaultInjector}
