be163177c6d0430dc768ecdd7a4444125a0afd9f
Alessio Colucci <alessio.colucci@gmail.com>
2021-02-26 19:14:05 +0100

    Implementation for Cell.

--- src/fi/hardwaremodel.py
+++ src/fi/hardwaremodel.py
@@ -8,45 +8,109 @@
 # this will work at different layers (physical, architectural, software)
 # for mapping the faults at multiple levels
 
+# for post-poned annotations, required only from Python 3.7 to 3.9
+from __future__ import annotations
+
 import collections
+import copy
 import dataclasses
 import enum
+import functools
 import typing
 
 from . import basefaultdescriptor
 
+# make a single enum, using Physical_Compute instead of Compute and so on
+# in this way we still use combinations Physical_Compute | Architectural_Register
+# but we have unique identifiers for dicts and stuff, otherwise we would have
+# collisions when numbering different enums
+CellType = enum.IntFlag('CellType',
+                        [
 
-PhysicalType = enum.IntFlag('PhysicalType',
-                            ['Compute',
-                             'Control',
-                             'StaticMemory',
-                             'DynamicMemory',
-                             'Interconnection',
-                             ],
-                            module=__name__)
-ArchitecturalType = enum.IntFlag('ArchitecturalType',
-                                 ['',
-                                  '',
-                                  '',
-                                  '',
-                                  '',
-                                  ],
-                                 module=__name__)
-SoftwareType = enum.IntFlag('SoftwareType',
-                            ['',
-                             '',
-                             '',
-                             '',
-                             '',
-                             ],
-                            module=__name__)
+                         # physical types
+                         'Physical_Compute',
+                         'Physical_Control',
+                         'Physical_StaticMemory',
+                         'Physical_DynamicMemory',
+                         'Physical_Interconnection',
 
-# we use these functions to convert the effects of a fault from a level to
-# another
-# for now the model is statistical, taken from other papers
-def physical_to_architectural(physical_type: PhysicalType,
-                              architectural_type: ArchitecturalType):
-    pass
+                         # architectural types
+                         'Architectural_',
+
+                         # software types
+                         'Software_',
+
+                        ],
+                        module=__name__)
+
+
+
+
+@dataclasses.dataclass(init=True)
+class Cell(object):
+    types: typing.List[enum.IntFlag, ...] = dataclasses.field(init=True,
+                                                        default_factory=list)
+    _tree_default_dict_factory = dataclasses.field(init=False,
+                    default=functools.partial(collections.defaultdict, list))
+    _dependency_default_dict_factory = dataclasses.field(init=False,
+                    default=functools.partial(collections.defaultdict, 0))
+
+    # these lists are used to have all the predecessors and successors for
+    # propagating the fault effects
+    # siblings is for cells at the same tree level
+    predecessors: typing.DefaultDict[CellType, typing.List[Cell, ...]] = \
+        dataclasses.field(init=True,
+                          default_factory=_tree_default_dict_factory)
+    siblings: typing.DefaultDict[CellType, typing.List[Cell, ...]] = \
+        dataclasses.field(init=True,
+                          default_factory=_tree_default_dict_factory)
+    successors: typing.DefaultDict[CellType, typing.List[Cell, ...]] = \
+        dataclasses.field(init=True,
+                          default_factory=_tree_default_dict_factory)
+
+    # these dicts associate each cell type to the propagation probability
+    # generally we consider only successors, but in case also predecessors and
+    # siblings can be considered
+    # each of them contain the probability [0, 1] to propagate the fault
+    dependency_predecessors: typing.DefaultDict[CellType, float] = \
+        dataclasses.field(init=True,
+                          default_factory=_dependency_default_dict_factory)
+    dependency_siblings: typing.DefaultDict[CellType, float] = \
+        dataclasses.field(init=True,
+                          default_factory=_dependency_default_dict_factory)
+    dependency_successors: typing.DefaultDict[CellType, float] = \
+        dataclasses.field(init=True,
+                          default_factory=_dependency_default_dict_factory)
+
+    def __post_init__(self):
+        pass
+
+    def add_sibling(self, celltype: CellType, cell: Cell):
+        self.siblings[celltype].append(cell)
+
+    def add_predecessor(self, celltype: CellType, cell: Cell):
+        self.predecessors[celltype].append(cell)
+
+    def add_successor(self, celltype: CellType, cell: Cell):
+        self.successors[celltype].append(cell)
+
+    def reset_siblings(self, celltype: typing.Union[CellType, None] = None):
+        if celltype is None:
+            self.siblings = self._tree_default_dict_factory()
+        del self.siblings[celltype]
+
+    def reset_predecessors(self, celltype: typing.Union[CellType, None] = None):
+        if celltype is None:
+            self.predecessors = self._tree_default_dict_factory()
+        del self.predecessors[celltype]
+
+    def reset_successors(self, celltype: typing.Union[CellType, None] = None):
+        if celltype is None:
+            self.successors = self._tree_default_dict_factory()
+        del self.successors[celltype]
+
+
+
 
 
 @dataclasses.dataclass(init=True)
@@ -55,9 +119,7 @@ class HardwareModel(object):
     chip_height: float
     chip_measurement_unit: str = "mm^2"
 
-    physical_cells: typing.Tuple[typing.Tuple[PhysicalType]]
-    architectural_cells: typing.Tuple[typing.Tuple[ArchitecturalType]]
-    software_cells: typing.Tuple[typing.Tuple[SoftwareType]]
+    cells: typing.Tuple[typing.Tuple[Cell, ...], ...]
 
     def __post_init__(self):
         pass
