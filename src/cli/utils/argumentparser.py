288265c3f91d1938daa55bfd6f7a222a9a5e9595
Alessio Colucci <alessio.colucci@gmail.com>
2021-06-03 12:53:18 +0200

    Remove excess code from ArgumentParser.

--- src/cli/utils/argumentparser.py
+++ src/cli/utils/argumentparser.py
@@ -1,14 +1,5 @@
 import argparse
-import pathlib
 
 
 class ArgumentParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.add_argument(
-                'config',
-                nargs='+',
-                action='extend',
-                type=pathlib.Path
-        )
+    pass
