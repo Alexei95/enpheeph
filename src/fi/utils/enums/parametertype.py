2f542533dd3f42c9122f215744085dd1e723b77f
Alessio Colucci <alessio.colucci@gmail.com>
2021-06-23 12:30:43 +0200

    Improve types for parameter injection.

--- src/fi/utils/enums/parametertype.py
+++ src/fi/utils/enums/parametertype.py
@@ -2,20 +2,38 @@ import enum
 
 
 class ParameterType(enum.Flag):
+
     # network type
     DNN = enum.auto()
     SNN = enum.auto()
+
+    # sub-network type, as we need special care for RNN
+    RNN = enum.auto()
+
     # parameter type
     Weight = enum.auto()
     Activation = enum.auto()
+    State = enum.auto()
+
+    # state types
+    LIFState = enum.auto()
+
+    # variables saved in state
+    Voltage = enum.auto()
+    Current = enum.auto()
+
     # tensor type
     Dense = enum.auto()
     Sparse = enum.auto()
+
     # sparse coordinates type
     COO = enum.auto()
+
     # sparse coordinates
     Index = enum.auto()
     Value = enum.auto()
+
     # complex types
     DNNWeightDense = DNN | Weight | Dense
     DNNActivationDense = DNN | Activation | Dense
+    SNNStateLIFStateVoltageDense = SNN | State | LIFState | Voltage | Dense
