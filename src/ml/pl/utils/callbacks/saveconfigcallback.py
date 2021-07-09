e201da87f9e402510506dbef55ca32307acc2eeb
Alessio Colucci <alessio.colucci@gmail.com>
2021-07-09 17:42:09 +0200

    Add versioning and time info in SaveConfigCallback
    Now in the configuration one can specify whether versioning
    should be done inside the configs directory.
    Depending on the argument, it will be treated as the version
    number or the latest version number incremented by one will be used.
    In addition also a time signature can be used.
    
    The basic config has been updated to reflect this.

--- src/ml/pl/utils/callbacks/saveconfigcallback.py
+++ src/ml/pl/utils/callbacks/saveconfigcallback.py
@@ -3,9 +3,12 @@ import os
 import pathlib
 import shutil
 import sys
+import time
 import typing
 
 import pytorch_lightning.callbacks
+
+import src.utils.functions
 
 
 class SaveConfigCallback(pytorch_lightning.callbacks.Callback):
@@ -73,12 +76,16 @@ class SaveConfigCallback(pytorch_lightning.callbacks.Callback):
             ]],
             dest_dir: typing.Union[pathlib.Path, str, bytes, os.PathLike],
             subdirectory: str = 'configs',
+            version: typing.Union[int, bool] = True,
+            time_info: bool = True,
     ):
         super().__init__()
 
         self.configs = [pathlib.Path(c) for c in configs]
         self.dest_dir = pathlib.Path(dest_dir)
         self.subdirectory = subdirectory
+        self.version = version
+        self.time_info = time_info
 
     def make_config(self):
         self.CONFIG_PATHS = {
@@ -98,7 +105,55 @@ class SaveConfigCallback(pytorch_lightning.callbacks.Callback):
         # we generate the complete destination file
         # and we make sure it exists
         complete_dest_dir = self.dest_dir.resolve() / self.subdirectory
-        print(self.dest_dir.resolve(), complete_dest_dir)
+        # we instantiate the list with the extra information for the
+        # subdirectory
+        extra_info_subdir = []
+        # version checks
+        if self.version is True:
+            versions = complete_dest_dir.glob('**')
+            # add more checks here for 0 and for other possible files
+            # add flag for inserting time info
+
+            # to convert the string name to integer
+            # we assume it is in the format (version)_[versionnumber]_time_info
+            # so we are interested in the second element
+            def converter(x):
+                return int(x.split('_')[1])
+
+            # we convert the list of file names into a list of integers
+            # if one of them cannot be converted, we skip it
+            converted_versions = []
+            for v in versions:
+                # we only check for directories
+                if v.is_dir():
+                    try:
+                        converted_versions.append(converter(v.name))
+                    except (ValueError, IndexError):
+                        pass
+
+            # we sort the remaining numbers
+            sorted_versions = sorted(converted_versions)
+            # if there are some, we get the last one and increase it,
+            # otherwise we use 0 if empty
+            if not len(sorted_versions):
+                next_version = 0
+            else:
+                next_version = sorted_versions[-1] + 1
+
+            extra_info_subdir.extend(['version', str(next_version)])
+        # if it is a number we use it directly
+        elif isinstance(self.version, int):
+            extra_info_subdir.append(str(self.version))
+
+        # time checks
+        # if we should add time info, we get the current UTC time
+        if self.time_info:
+            time_info = src.utils.functions.current_utctime_string()
+            extra_info_subdir.append(time_info)
+        # here we join the extra sub-components
+        # if the list is empty, it will return the same directory when parsed
+        # with pathlib
+        complete_dest_dir = complete_dest_dir / '_'.join(extra_info_subdir)
         complete_dest_dir.mkdir(exist_ok=True, parents=True)
         # we cycle through the configurations
         for index, config in enumerate(self.configs):
