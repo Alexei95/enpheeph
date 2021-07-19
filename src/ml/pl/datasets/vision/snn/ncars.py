b621cd0183dd228faf8d5add3005ce6d97dadddc
Alessio Colucci <alessio.colucci@gmail.com>
2021-07-19 18:18:26 +0200

    Implement N-CARS with neuromorphic DM.

---
+++ src/ml/pl/datasets/vision/snn/ncars.py
@@ -0,0 +1,33 @@
+import copy
+import functools
+import pathlib
+
+import typing
+
+import pl_bolts
+import pytorch_lightning
+import tonic
+import tonic.datasets.ncars
+import torch
+
+import src.ml.pl.datasets.utils.monkeypatchedtonicdataset
+import src.ml.pl.datasets.vision.snn.neuromorphicdatamodule
+
+
+class NCARSDataModule(
+        src.ml.pl.datasets.vision.snn.neuromorphicdatamodule.
+        NeuromorphicDataModule
+):
+
+    dataset_cls = src.ml.pl.datasets.utils.\
+        monkeypatchedtonicdataset.monkey_patching_tonic_dataset(
+                tonic.datasets.ncars.NCARS,
+                tonic.datasets.ncars,
+                'ncars-train',
+                'ncars-test',
+        )
+    name = "N-CARS"
+    dims = (2, 120, 100)
+
+    def default_transforms(self) -> typing.Callable:
+        return tonic.transforms.Compose()
