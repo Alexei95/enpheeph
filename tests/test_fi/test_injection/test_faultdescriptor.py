99341ee92e2fa570a3f46166e19298111217357c
Alessio Colucci <alessio.colucci@gmail.com>
2021-07-26 12:19:25 +0200

    Add more parametrized tests for FaultDescriptor.
    tensor_index_conversion is not implemented yet.

--- tests/test_fi/test_injection/test_faultdescriptor.py
+++ tests/test_fi/test_injection/test_faultdescriptor.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 import src.fi.injection.faultdescriptor
@@ -97,17 +99,468 @@ class TestFaultDescriptor:
                     bit_value=bit_value,
             )
 
-    def test_init_bit_index(self):
-        assert 0
+    @pytest.mark.parametrize(
+        'parameter_type',
+        [
+            pytest.param(
+                    parameter_type,
+                    id=repr(parameter_type),
+            )
+            for parameter_type in (
+                    src.fi.utils.enums.parametertype.ParameterType
+            )
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_value',
+        [
+            pytest.param(
+                bit_value,
+                id=bit_value.name
+            )
+            for bit_value in src.fi.utils.enums.bitvalue.BitValue
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_index',
+        [
+            pytest.param(
+                bit_index,
+                id=str(bit_index)
+            )
+            for bit_index in ([], [slice(10), ..., 1, 10])
+        ]
+    )
+    def test_init_bit_index_tuple_conversion(
+            self,
+            parameter_type,
+            bit_index,
+            bit_value
+    ):        # it can be omitted only for SNN state or activation injection
+        assert src.fi.injection.faultdescriptor.FaultDescriptor(
+                module_name='',
+                parameter_type=parameter_type,
+                parameter_name="",
+                tensor_index=[],
+                bit_index= bit_index,
+                bit_value=bit_value,
+        ).bit_index == tuple(bit_index)
 
-    def test_init_tensor_index(self):
-        assert 0
+    @pytest.mark.parametrize(
+        'parameter_type',
+        [
+            pytest.param(
+                    parameter_type,
+                    id=repr(parameter_type),
+            )
+            for parameter_type in (
+                    src.fi.utils.enums.parametertype.ParameterType
+            )
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_value',
+        [
+            pytest.param(
+                bit_value,
+                id=bit_value.name
+            )
+            for bit_value in src.fi.utils.enums.bitvalue.BitValue
+        ]
+    )
+    @pytest.mark.parametrize(
+        'tensor_index',
+        [
+            pytest.param(
+                tensor_index,
+                id=str(tensor_index)
+            )
+            for tensor_index in ([], [slice(10), ..., 1, 10])
+        ]
+    )
+    def test_init_tensor_index_tuple_conversion(
+            self,
+            parameter_type,
+            tensor_index,
+            bit_value
+    ):
+        assert src.fi.injection.faultdescriptor.FaultDescriptor(
+                module_name='',
+                parameter_type=parameter_type,
+                parameter_name="",
+                tensor_index=tensor_index,
+                bit_index=[],
+                bit_value=bit_value,
+        ).tensor_index == tuple(tensor_index)
 
-    def test_hashability(self):
-        assert 0
+    @pytest.mark.parametrize(
+        'parameter_type',
+        [
+            pytest.param(
+                    parameter_type,
+                    id=repr(parameter_type),
+            )
+            for parameter_type in (
+                    src.fi.utils.enums.parametertype.ParameterType
+            )
+        ]
+    )
+    @pytest.mark.parametrize(
+        'tensor_index,correct_tensor_index',
+        [
+            pytest.param(
+                tensor_index,
+                (
+                        tuple(tensor_index)
+                        if isinstance(tensor_index, list)
+                        else tensor_index
+                ),
+                id=str(tensor_index)
+            )
+            for tensor_index in ([], [slice(10), ..., 1, 10], ...)
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_index,correct_bit_index',
+        [
+            pytest.param(
+                bit_index,
+                tuple(bit_index) if isinstance(bit_index, list) else bit_index,
+                id=str(bit_index)
+            )
+            for bit_index in ([], [slice(10), ..., 1, 10], ...)
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_value',
+        [
+            pytest.param(
+                bit_value,
+                id=bit_value.name
+            )
+            for bit_value in src.fi.utils.enums.bitvalue.BitValue
+        ]
+    )
+    def test_init(
+            self,
+            parameter_type,
+            tensor_index,
+            correct_tensor_index,
+            bit_index,
+            correct_bit_index,
+            bit_value
+    ):
+        values = {
+                'module_name': (
+                        ''.join(
+                                str(random.randint(0, n))
+                                for n in range(random.randint(0, 100))
+                        )
+                ),
+                'parameter_type': parameter_type,
+                'parameter_name': (
+                        ''.join(
+                                str(random.randint(0, n))
+                                for n in range(random.randint(1, 100))
+                        )
+                ),
+                'tensor_index': tensor_index,
+                'bit_index': bit_index,
+                'bit_value': bit_value,
+        }
+        fd = src.fi.injection.faultdescriptor.FaultDescriptor(
+                **values
+        )
+        correct_values = values.copy()
+        correct_values.update(
+                {
+                        'bit_index': correct_bit_index,
+                        'tensor_index': correct_tensor_index,
+                }
+        )
 
-    def test_bit_index_coversion(self):
-        assert 0
+        for name, value in correct_values.items():
+            assert getattr(fd, name) == value
 
-    def test_tensor_index_coversion(self):
-        assert 0
+    @pytest.mark.parametrize(
+        'parameter_type',
+        [
+            pytest.param(
+                    parameter_type,
+                    id=repr(parameter_type),
+            )
+            for parameter_type in (
+                    src.fi.utils.enums.parametertype.ParameterType
+            )
+        ]
+    )
+    @pytest.mark.parametrize(
+        'tensor_index',
+        [
+            pytest.param(
+                tensor_index,
+                id=str(tensor_index)
+            )
+            for tensor_index in ([], [slice(10), ..., 1, 10], ...)
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_index',
+        [
+            pytest.param(
+                bit_index,
+                id=str(bit_index)
+            )
+            for bit_index in ([], [slice(10), ..., 1, 10], ...)
+        ]
+    )
+    @pytest.mark.parametrize(
+        'bit_value',
+        [
+            pytest.param(
+                bit_value,
+                id=bit_value.name
+            )
+            for bit_value in src.fi.utils.enums.bitvalue.BitValue
+        ]
+    )
+    def test_hashability(
+            self,
+            parameter_type,
+            tensor_index,
+            bit_index,
+            bit_value
+    ):
+        values_one = {
+                'module_name': (
+                        ''.join(
+                                str(random.randint(0, n))
+                                for n in range(random.randint(0, 100))
+                        )
+                ),
+                'parameter_type': parameter_type,
+                'parameter_name': (
+                        ''.join(
+                                str(random.randint(0, n))
+                                for n in range(random.randint(0, 100))
+                        )
+                ),
+                'tensor_index': tensor_index,
+                'bit_index': bit_index,
+                'bit_value': bit_value,
+        }
+        fd_one = src.fi.injection.faultdescriptor.FaultDescriptor(
+                **values_one
+        )
+
+        values_two = {
+                'module_name': (
+                        ''.join(
+                                str(random.randint(0, n))
+                                for n in range(random.randint(0, 100))
+                        )
+                ),
+                'parameter_type': parameter_type,
+                'parameter_name': (
+                        ''.join(
+                                str(random.randint(0, n))
+                                for n in range(random.randint(0, 100))
+                        )
+                ),
+                'tensor_index': tensor_index,
+                'bit_index': bit_index,
+                'bit_value': bit_value,
+        }
+        fd_two = src.fi.injection.faultdescriptor.FaultDescriptor(
+                **values_two
+        )
+
+        assert hash(fd_one) != hash(fd_two)
+
+    @pytest.mark.parametrize(
+        'bit_index,bit_width,converted_bit_index',
+        [
+            pytest.param(
+                    [],
+                    32,
+                    (),
+                    id=str(([], 32))
+            ),
+            pytest.param(
+                    slice(0, 10, 2),
+                    16,
+                    (0, 2, 4, 6, 8),
+                    id=str((slice(0, 10, 2), 16))
+            ),
+            pytest.param(
+                    slice(0, 10, 2),
+                    7,
+                    (0, 2, 4, 6),
+                    id=str((slice(0, 10, 2), 7))
+            ),
+            pytest.param(
+                    [0, 2, 10],
+                    16,
+                    (0, 2, 10),
+                    id=str(([0, 2, 10], 16))
+            ),
+            pytest.param(
+                    [0, 2, 10],
+                    7,
+                    (0, 2),
+                    id=str(([0, 2, 10], 7))
+            ),
+            pytest.param(
+                    ...,
+                    7,
+                    (0, 1, 2, 3, 4, 5, 6),
+                    id=str((..., 7))
+            ),
+            pytest.param(
+                    [1, 1, 2, 0, 4, 7, 5, 1, 1, 1, 1, 1],
+                    10,
+                    (0, 1, 2, 4, 5, 7),
+                    id=str(([1, 1, 2, 0, 4, 7, 5, 1, 1, 1, 1, 1], 10))
+            ),
+        ]
+    )
+    def test_bit_index_coversion(
+            self,
+            bit_index,
+            bit_width,
+            converted_bit_index
+    ):
+        conv_bit_index = (
+                src.fi.injection.faultdescriptor.
+                FaultDescriptor.bit_index_conversion(
+                        bit_index,
+                        bit_width
+                )
+        )
+        assert conv_bit_index == converted_bit_index
+
+    @pytest.mark.parametrize(
+        'bit_index,bit_width',
+        [
+            pytest.param(
+                    [],
+                    32,
+                    (),
+                    id=str(([], 32))
+            ),
+            pytest.param(
+                    slice(0, 10, 2),
+                    16,
+                    (0, 2, 4, 6, 8),
+                    id=str((slice(0, 10, 2), 16))
+            ),
+            pytest.param(
+                    slice(0, 10, 2),
+                    7,
+                    (0, 2, 4, 6),
+                    id=str((slice(0, 10, 2), 7))
+            ),
+            pytest.param(
+                    [0, 2, 10],
+                    16,
+                    (0, 2, 10),
+                    id=str(([0, 2, 10], 16))
+            ),
+            pytest.param(
+                    [0, 2, 10],
+                    7,
+                    (0, 2),
+                    id=str(([0, 2, 10], 7))
+            ),
+            pytest.param(
+                    ...,
+                    7,
+                    (0, 1, 2, 3, 4, 5, 6),
+                    id=str((..., 7))
+            ),
+            pytest.param(
+                    [1, 1, 2, 0, 4, 7, 5, 1, 1, 1, 1, 1],
+                    10,
+                    (0, 1, 2, 4, 5, 7),
+                    id=str(([1, 1, 2, 0, 4, 7, 5, 1, 1, 1, 1, 1], 10))
+            ),
+        ]
+    )
+    def test_bit_index_coversion_value_error(
+            self,
+            bit_index,
+            bit_width
+    ):
+        with pytest.raises(ValueError):
+            (
+                    src.fi.injection.faultdescriptor.
+                    FaultDescriptor.bit_index_conversion(
+                            bit_index,
+                            bit_width
+                    )
+            )
+
+    @pytest.mark.skip("Not ready yet")
+    @pytest.mark.parametrize(
+        'tensor_index,tensor_shape,converted_tensor_index',
+        [
+            pytest.param(
+                    [],
+                    32,
+                    (),
+                    id=str(([], 32))
+            ),
+            pytest.param(
+                    slice(0, 10, 2),
+                    16,
+                    (0, 2, 4, 6, 8),
+                    id=str((slice(0, 10, 2), 16))
+            ),
+            pytest.param(
+                    slice(0, 10, 2),
+                    7,
+                    (0, 2, 4, 6),
+                    id=str((slice(0, 10, 2), 7))
+            ),
+            pytest.param(
+                    [0, 2, 10],
+                    16,
+                    (0, 2, 10),
+                    id=str(([0, 2, 10], 16))
+            ),
+            pytest.param(
+                    [0, 2, 10],
+                    7,
+                    (0, 2),
+                    id=str(([0, 2, 10], 7))
+            ),
+            pytest.param(
+                    ...,
+                    7,
+                    (0, 1, 2, 3, 4, 5, 6),
+                    id=str((..., 7))
+            ),
+            pytest.param(
+                    [1, 1, 2, 0, 4, 7, 5, 1, 1, 1, 1, 1],
+                    10,
+                    (0, 1, 2, 4, 5, 7),
+                    id=str(([1, 1, 2, 0, 4, 7, 5, 1, 1, 1, 1, 1], 10))
+            ),
+        ]
+    )
+    def test_tensor_index_coversion(
+            self,
+            tensor_index,
+            tensor_shape,
+            force_index,
+            converted_tensor_index
+    ):
+        conv_tensor_index = (
+                src.fi.injection.faultdescriptor.
+                FaultDescriptor.tensor_index_conversion(
+                        tensor_index,
+                        tensor_shape,
+                        force_index
+                )
+        )
+        assert conv_tensor_index == converted_tensor_index
