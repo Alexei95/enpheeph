2b8e13376f71ee5a413978ecb232fffbcbca83ea
Alessio Colucci <alessio.colucci@gmail.com>
2021-08-22 20:35:47 +0200

    Remove configs and coda for models.
    These were split in a different repository, pynndora.

--- pynndora/ml/pl/datasets/utils/collate/totensor.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-import src.ml.pl.datasets.utils.collate.collateabc
-
-
-class ToTensor(src.ml.pl.datasets.utils.collate.collateabc.CollateABC):
-    def call(self, input_, target):
-        return torch.as_tensor(input_), torch.as_tensor(target)
--- pynndora/ml/pl/datasets/utils/transforms/totensor.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-import src.ml.pl.datasets.utils.transforms.transformabc
-
-
-class ToTensor(src.ml.pl.datasets.utils.transforms.transformabc.TransformABC):
-    def call(self, element):
-        return torch.as_tensor(element)
