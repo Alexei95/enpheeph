9ab9db3362fd933d9db161a291b61aa6477274e2
Alexei95 <1548472+Alexei95@users.noreply.github.com>
2022-01-29 22:20:00 +0100

    feat(injection): add Fault and Monitor ABCs
    FaultABC and MonitorABC are used to distinguish faults and monitors,
    they have no other uses for now.

---
+++ src/enpheeph/injections/faultabc.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import enpheeph.injections.injectionabc
+
+
+class FaultABC(enpheeph.injections.injectionabc.InjectionABC):
+    pass
---
+++ src/enpheeph/injections/monitorabc.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import enpheeph.injections.injectionabc
+
+
+class MonitorABC(enpheeph.injections.injectionabc.InjectionABC):
+    pass
