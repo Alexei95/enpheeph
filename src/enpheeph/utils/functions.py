5a52a5218065ef11cd8c1b062125a08d9bee9f8b
Alexei95 <1548472+Alexei95@users.noreply.github.com>
2022-02-03 00:38:52 +0100

    fix(functions): safer check on __module__
    __module__ might not be properly defined in some classes,
    so we check if it is a string before post-processing it.

--- src/enpheeph/utils/functions.py
+++ src/enpheeph/utils/functions.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# enpheeph - Neural Fault Injection Framework
+# Copyright (C) 2020-2022 Alessio "Alexei95" Colucci
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import re
 import typing
 
@@ -17,4 +33,5 @@ def camel_to_snake(camel: str) -> str:
 
 def get_object_library(obj: typing.Any) -> typing.Optional[str]:
     module = getattr(obj.__class__, "__module__", None)
-    return module.split(".")[0] if module is not None else None
+    # to be safe we return None if the module is not a string
+    return module.split(".")[0] if isinstance(module, str) else None
