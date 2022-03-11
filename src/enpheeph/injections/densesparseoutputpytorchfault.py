45641d727ca10910384556d9b845904f03145afe
Alexei95 <1548472+Alexei95@users.noreply.github.com>
2022-03-11 15:27:15 +0100

    feat: update Sparse injection
    Support for mask injections.
    
    Improve support for proper update of value/index pairs.
    
    Add support for indexing.

--- src/enpheeph/injections/densesparseoutputpytorchfault.py
+++ src/enpheeph/injections/densesparseoutputpytorchfault.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
+# enpheeph - Neural Fault Injection Framework
+# Copyright (C) 2020-2022 Alessio "Alexei95" Colucci
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import typing
 
+import enpheeph.injections.faultabc
 import enpheeph.injections.pytorchinjectionabc
 import enpheeph.injections.mixins.pytorchmaskmixin
 import enpheeph.injections.mixins.pytorchsparseinterfacemixin
+import enpheeph.injections.mixins.pytorchtensorobjectvalidatormixin
 import enpheeph.injections.plugins.mask.lowleveltorchmaskpluginabc
 import enpheeph.utils.data_classes
 
@@ -13,9 +31,16 @@ if typing.TYPE_CHECKING:
 
 
 class DenseSparseOutputPyTorchFault(
+    enpheeph.injections.faultabc.FaultABC,
     enpheeph.injections.pytorchinjectionabc.PyTorchInjectionABC,
     enpheeph.injections.mixins.pytorchmaskmixin.PyTorchMaskMixin,
     enpheeph.injections.mixins.pytorchsparseinterfacemixin.PyTorchSparseInterfaceMixin,
+    (
+        # fmt: off
+        enpheeph.injections.mixins.
+        pytorchtensorobjectvalidatormixin.PyTorchTensorObjectValidatorMixin
+        # fmt: on
+    ),
 ):
     location: enpheeph.utils.data_classes.FaultLocation
     low_level_plugin: (
@@ -29,6 +54,9 @@ class DenseSparseOutputPyTorchFault(
 
     def __init__(
         self,
+        indexing_plugin: (
+            enpheeph.injections.plugins.indexing.indexingpluginabc.IndexingPluginABC
+        ),
         location: enpheeph.utils.data_classes.FaultLocation,
         low_level_torch_plugin: (
             # black has issues with long names
@@ -40,6 +68,7 @@ class DenseSparseOutputPyTorchFault(
     ) -> None:
         super().__init__()
 
+        self.indexing_plugin = indexing_plugin
         self.location = location
         self.low_level_plugin = low_level_torch_plugin
 
@@ -58,23 +87,20 @@ class DenseSparseOutputPyTorchFault(
     ) -> None:
         target = self.get_sparse_injection_parameter(output)
 
-        # using this indexing we cover only the main dimensions, as it is difficult
-        # to select a specific batch on the sparse coordinates
-        # sparse coordinates are [number of dimensions, number of elements]
         self.indexing_plugin.select_active_dimensions(
-            [
-                enpheeph.utils.enums.DimensionType.Tensor,
-            ],
+            [enpheeph.utils.enums.DimensionType.Tensor],
             autoshift_to_boundaries=True,
-            fill_empty_index=False,
         )
-        # we don't need to return the tensors as all the changes are in-place
-        self.generate_mask(target, tensor_only=None)
 
-        self.inject_mask(target, tensor_only=None)
+        self.generate_mask(target, tensor_only=None, force_recompute=True)
 
-        # we reset the active dimensions
+        target = self.inject_mask(target, tensor_only=None)
+
+        output = self.set_sparse_injection_parameter(output, target).to_dense()
+
         self.indexing_plugin.reset_active_dimensions()
+
+        return output
 
     def setup(
         self,
--- src/enpheeph/injections/mixins/pytorchsparseinterfacemixin.py
+++ src/enpheeph/injections/mixins/pytorchsparseinterfacemixin.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# enpheeph - Neural Fault Injection Framework
+# Copyright (C) 2020-2022 Alessio "Alexei95" Colucci
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import abc
 import typing
 
@@ -43,11 +59,50 @@ class PyTorchSparseInterfaceMixin(abc.ABC):
         sparse_value_flag = (
             self.location.parameter_type.Sparse | self.location.parameter_type.Value
         )
-        if sparse_index_flag.value in self.location.parameter_type:
+        if sparse_index_flag in self.location.parameter_type:
             target = sparse_output.indices()
-        elif sparse_value_flag.value in self.location.parameter_type:
+        elif sparse_value_flag in self.location.parameter_type:
             target = sparse_output.values()
         else:
             raise ValueError("This operation is not supported with sparse tensors")
 
         return target
+
+    def set_sparse_injection_parameter(
+        self,
+        target: "torch.Tensor",
+        new_value: "torch.Tensor",
+    ) -> "torch.Tensor":
+        import torch
+
+        sparse_output = target.to_sparse()
+
+        # mypy has some issues in recognizing the enum names if taken from a name itself
+        # e.g. A.a.a
+        # we use separate values to avoid this issue
+        # however we still require typing from the enum,
+        # which limits the customizability of the interface, as before it could be any
+        # compatible enum but now it must be this specific one
+        # **NOTE**: a possible alternative is using .value at the end to extract the
+        # correct enum, which does nothing
+        # however value returns the integer value, so it is still not a clean trick
+        sparse_index_flag = (
+            self.location.parameter_type.Sparse | self.location.parameter_type.Index
+        )
+        sparse_value_flag = (
+            self.location.parameter_type.Sparse | self.location.parameter_type.Value
+        )
+        if sparse_index_flag in self.location.parameter_type:
+            other_target_value = sparse_output.values()
+            new_target = torch.sparse_coo_tensor(
+                indices=new_value, values=other_target_value
+            )
+        elif sparse_value_flag in self.location.parameter_type:
+            other_target_value = sparse_output.indices()
+            new_target = torch.sparse_coo_tensor(
+                indices=other_target_value, values=new_value
+            )
+        else:
+            raise ValueError("This operation is not supported with sparse tensors")
+
+        return new_target
