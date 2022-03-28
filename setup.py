afe3fe04bb8ac8d94b099efd41ff9a06e6780c65
Alexei95 <1548472+Alexei95@users.noreply.github.com>
2022-03-28 19:56:32 +0200

    feat: use pyproject.toml for setuptools
    Fix version in about.
    
    Add separate CLASSIFIERS file.
    
    Remove old files from merge.
    
    Clean setup.py, all the info is in pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,84 +1,22 @@
 # -*- coding: utf-8 -*-
+# enpheeph - Neural Fault Injection Framework
+# Copyright (C) 2020-2022 Alessio "Alexei95" Colucci
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # import importlib
 import setuptools
 
-# about = importlib.util.
-
-extras_require = {
-    "full": [
-        "cupy >= 9.0.0",
-        "norse >= 0.0.7",
-        "numpy >= 1.19",
-        "pytorch-lightning >= 1.5",
-        "sqlalchemy >= 1.4.20",
-        "torch >= 1.8, < 1.10",
-    ],
-    "dev": [
-        "black",
-        "coverage[toml]",
-        "flake8",
-        "mypy",
-        "pre-commit",
-        "pytest >= 6.2.0",  # 6.2.5 for Python 3.10+
-        "pytest-cov",
-        "pytest-xdist",
-    ],
-}
-extras_require["full-dev"] = extras_require["full"] + extras_require["dev"]
-
 if __name__ == "__main__":
-    setuptools.setup(
-        name="enpheeph",
-        description="",
-        version="0.0.1",
-        # long_description=,
-        # long_description_content_type=,
-        # keywords=,
-        # author=,
-        # author_email=,
-        # url="https://github.com/psf/black",
-        # project_urls={
-        #     "Changelog": "https://github.com/psf/black/blob/main/CHANGES.md",
-        # },
-        # license="MIT",
-        packages=setuptools.find_packages(where="src"),
-        # if packages are inside src recursively
-        package_dir={"": "src"},
-        # for py.typed and PEP-517 compatibility
-        # package_data={
-        #     "blib2to3": ["*.txt"],
-        #     "black": ["py.typed"],
-        #     "black_primer": ["primer.json"],
-        # },
-        python_requires=">=3.8",
-        zip_safe=False,
-        # dependencies
-        install_requires=[
-            "setuptools >= 58.0",
-            "packaging >= 20.0",
-        ],
-        extras_require=extras_require,
-        # test_suite="tests.test_black",
-        # classifiers=[
-        #     "Development Status :: 4 - Beta",
-        #     "Environment :: Console",
-        #     "Intended Audience :: Developers",
-        #     "License :: OSI Approved :: MIT License",
-        #     "Operating System :: OS Independent",
-        #     "Programming Language :: Python",
-        #     "Programming Language :: Python :: 3.6",
-        #     "Programming Language :: Python :: 3.7",
-        #     "Programming Language :: Python :: 3.8",
-        #     "Programming Language :: Python :: 3.9",
-        #     "Programming Language :: Python :: 3 :: Only",
-        #     "Topic :: Software Development :: Libraries :: Python Modules",
-        #     "Topic :: Software Development :: Quality Assurance",
-        # ],
-        # entry_points={
-        #     "console_scripts": [
-        #         "black=black:patched_main",
-        #         "blackd=blackd:patched_main [d]",
-        #         "black-primer=black_primer.cli:main",
-        #     ]
-        # },
-    )
+    setuptools.setup()
--- src/enpheeph/__about__.py
+++ src/enpheeph/__about__.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
+# enpheeph - Neural Fault Injection Framework
+# Copyright (C) 2020-2022 Alessio "Alexei95" Colucci
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # taken as example from PyTorch Lightning
 
 # use importlib.metadata to gather the info from the package information
@@ -9,3 +25,6 @@ import time
 # these two variables will not be imported with import * as they start with _
 _this_year = time.strftime("%Y")
 _start_year = "2020"
+
+# version, to be accessed by setuptools
+__version__ = "0.0.1a1"
