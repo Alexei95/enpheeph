5cdde593f01482952a13b30acb72d942dfd912e0
Alexei95 <Alexei95@users.noreply.github.com>
2022-08-12 16:52:51 +0000

    CI - mkinit: Update __init__.py recursively

--- src/enpheeph/helpers/summaries/abc/__init__.py
+++ src/enpheeph/helpers/summaries/abc/__init__.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 def lazy_import(module_name, submodules, submod_attrs):
     import importlib
     import os
